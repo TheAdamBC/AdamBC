0e61c9cfc747c869c3068ac16084da090cd99c1b
Vakindu Philliam <Pvakindu@gmail.com>
2022-06-24 04:22:31 +0300

    feat(dapp-examples): created new dapp `image_detect_shapes`.

---
+++ app/dapp_examples/py/media_analysis/image_detect_shapes/App.py
@@ -0,0 +1,119 @@
+#**
+# * The Decentralized App (DApp):
+# * This is where the App developer writes the decentralized app.
+# * Make sure the code is written within the specified space region.
+# * 
+# * IMPORTANT: 
+# * 1. Developer DApp CODE MUST BE WRITTEN WITHIN SPECIFIED SPACE REGION.
+# * 2. DApp MUST return values through the 'results' variable.
+# * 3. DApp MUST RETURN A JSON Object.
+# * 4. DApp data crunching should not exceed 100MB of Data per peer task.
+# * 5. If you change the name of 'results', make sure to change it at DApp's 'return results' code.
+# * 
+# *
+
+import sys, json
+
+results = {} # Storage for successful results.
+
+json_str = input() # Capture data input
+
+params = json.loads(json_str) # Load parameters values (params) to process
+
+#*********************************************************************************/
+#                 /* START WRITING YOUR DAPP CODE BEGINNING HERE: */
+#*********************************************************************************/
+
+# EXAMPLE:
+# Identifying shapes inside an image.
+# Import necessary DApp resources, scripts, assets and modules needed for the task.
+from PIL import Image
+import numpy as np
+import cv2
+import os
+import base64
+
+# Variable to store shapes found
+shapes = {}
+
+fileName = params['uParams'][0]['parameter2'] # Capture name of file
+fileData = base64.b64decode(params['uParams'][0]['parameter1']) # Capture file
+
+# Parse image file to Numpy array
+img_buffer = np.frombuffer(fileData, dtype=np.uint8)
+im = cv2.imdecode(img_buffer, flags=1)
+
+# Save file to local directory
+try:
+    cv2.imwrite(os.path.join('app/assets/media/', f'{fileName}'), im)
+    cv2.waitKey(0)
+except:
+    print('Problem saving file!')
+
+# Load image and convert to HSV
+try:
+    img = cv2.imread(f'app/assets/media/{fileName}')
+except:
+    print('Error processing file!')
+
+# converting image into grayscale image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  
+# setting threshold of gray image
+_, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+  
+# using a findContours() function
+contours, _ = cv2.findContours(
+    threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  
+i = 0
+  
+# list for storing names of shapes
+for contour in contours:
+  
+    # here we are ignoring first counter because 
+    # findcontour function detects whole image as shape
+    if i == 0:
+        i = 1
+        continue
+  
+    # cv2.approxPloyDP() function to approximate the shape
+    approx = cv2.approxPolyDP(
+        contour, 0.01 * cv2.arcLength(contour, True), True)
+      
+    # using drawContours() function
+    cv2.drawContours(img, [contour], 0, (0, 0, 255), 5)
+  
+    # finding center point of shape
+    M = cv2.moments(contour)
+    if M['m00'] != 0.0:
+        x = int(M['m10']/M['m00'])
+        y = int(M['m01']/M['m00'])
+  
+    # putting shape name at center of each shape
+    if len(approx) == 3:
+        shapes['Triangle']="3 Sides"
+  
+    elif len(approx) == 4:
+        shapes['Quadrilateral']="4 Sides"
+  
+    elif len(approx) == 5:
+        shapes['Pentagon']="5 Sides"
+  
+    elif len(approx) == 6:
+        shapes['Hexagon']="6 Sides"
+  
+    else:
+        shapes['Circle']=(x,y)
+  
+# Return results of processing
+results=shapes
+
+#*********************************************************************************/
+#                 /* STOP WRITING YOUR DAPP CODE UP UNTIL HERE.*/
+#*********************************************************************************/                 
+
+# Results must return valid JSON Object
+print(results)
+sys.stdout.flush()
+
